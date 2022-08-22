be88bcf1a22e5c3126bab520fa38095178d8f9db
Stephen Benjamin <stephen@redhat.com>
2022-08-22 13:40:56 -0400

    Sync pull request data from GitHub
    This change links pull requests to job runs, allowing us to do
    additional analysis. Pull requests are linked as both the PR being
    tested, as well as the specific SHA -- meaning the analysis can be done
    on commits that actually merged allow us to identify the most painful
    and frequent failures.

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"gopkg.in/yaml.v3"
 
 	v1 "github.com/openshift/sippy/pkg/apis/config/v1"
+	"github.com/openshift/sippy/pkg/prowloader/github"
 	"github.com/openshift/sippy/pkg/sippyserver/metrics"
 
 	"github.com/openshift/sippy/pkg/buganalysis"
@@ -71,6 +72,7 @@ type Options struct {
 	LogLevel                           string
 	LoadTestgrid                       bool
 	LoadProw                           bool
+	LoadGitHub                         bool
 	Config                             string
 	GoogleServiceAccountCredentialFile string
 	GoogleOAuthClientCredentialFile    string
@@ -127,6 +129,7 @@ func main() {
 	flags.BoolVar(&opt.LoadTestgrid, "load-testgrid", true, "Fetch job and job run data from testgrid")
 
 	flags.BoolVar(&opt.LoadProw, "load-prow", opt.LoadProw, "Fetch job and job run data from prow")
+	flags.BoolVar(&opt.LoadGitHub, "load-github", opt.LoadGitHub, "Fetch PR state data from GitHub, only for use with Prow-based Sippy")
 	flags.StringVar(&opt.Config, "config", opt.Config, "Configuration file for Sippy, required if using Prow-based Sippy")
 
 	// google cloud creds
@@ -367,7 +370,12 @@ func (o *Options) Run() error {
 				return err
 			}
 
-			prowLoader := prowloader.New(dbc, gcsClient, "origin-ci-test", o.getVariantManager(), o.getSyntheticTestManager(), o.OpenshiftReleases, &sippyConfig)
+			var githubClient *github.Client
+			if o.LoadGitHub {
+				githubClient = github.New(context.TODO())
+			}
+
+			prowLoader := prowloader.New(dbc, gcsClient, "origin-ci-test", githubClient, o.getVariantManager(), o.getSyntheticTestManager(), o.OpenshiftReleases, &sippyConfig)
 			if err := prowLoader.LoadProwJobsToDB(); err != nil {
 				return err
 			}
