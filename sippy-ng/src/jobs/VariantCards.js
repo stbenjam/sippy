f621f97dbc7bbf70060ee7da6bcd9681734f041b
Stephen Benjamin <stephen@redhat.com>
2024-06-10 14:23:11 -0400

    Use table instead of variant cards

--- sippy-ng/src/jobs/VariantCards.js
+++ sippy-ng/src/jobs/VariantCards.js
@@ -1,21 +1,43 @@
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Tooltip,
-  Typography,
-} from '@mui/material'
-import { ExpandMore } from '@mui/icons-material'
+import './JobTable.css'
+import { Container, Link } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { generateClasses } from '../datagrid/utils'
+import { JOB_THRESHOLDS, VARIANT_THRESHOLDS } from '../constants'
+import { makeStyles, styled, withStyles } from '@mui/styles'
 import { pathForVariantAnalysis } from '../helpers'
 import { PropTypes } from 'prop-types'
-import { VARIANT_THRESHOLDS } from '../constants'
 import Alert from '@mui/material/Alert'
-import Grid from '@mui/material/Unstable_Grid2'
-import Info from '@mui/icons-material/Info'
-import MiniCard from '../components/MiniCard'
+import PassRateIcon from '../components/PassRateIcon'
 import React, { Fragment, useEffect } from 'react'
 
-export default function VariantCards(props) {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .wrapHeader .MuiDataGrid-columnHeaderTitle': {
+      textOverflow: 'ellipsis',
+      display: '-webkit-box',
+      '-webkit-line-clamp': 2,
+      '-webkit-box-orient': 'vertical',
+      overflow: 'hidden',
+      overflowWrap: 'break-word',
+      lineHeight: '20px',
+      whiteSpace: 'normal',
+    },
+    backdrop: {
+      zIndex: 999999,
+      color: '#fff',
+    },
+  },
+}))
+
+const SimpleLink = styled(Link)({
+  textDecoration: 'none',
+  color: 'inherit',
+})
+
+function VariantCards(props) {
+  const { classes } = props
+  const gridClasses = useStyles()
+
   const [variants, setVariants] = React.useState([])
   const [isLoaded, setLoaded] = React.useState(false)
   const [fetchError, setFetchError] = React.useState('')
@@ -53,69 +75,114 @@ export default function VariantCards(props) {
     return <p>Loading...</p>
   }
 
-  const minicard = (variant, index) => {
-    return (
-      <Grid key={index} md={2} sm={4}>
-        <MiniCard
-          link={pathForVariantAnalysis(props.release, variant.name)}
-          threshold={VARIANT_THRESHOLDS}
-          name={variant.name}
-          current={variant.current_pass_percentage}
-          currentRuns={variant.current_runs}
-          previous={variant.previous_pass_percentage}
-          previousRuns={variant.previous_runs}
-          tooltip={
-            'Current runs: ' +
-            variant.current_runs +
-            ', previous runs: ' +
-            variant.previous_runs
-          }
-        />
-      </Grid>
-    )
-  }
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Variant',
+      flex: 2,
+      renderCell: (params) => {
+        return (
+          <SimpleLink to={pathForVariantAnalysis(props.release, params.value)}>
+            {params.value}
+          </SimpleLink>
+        )
+      },
+    },
+    {
+      headerName: 'Current runs',
+      field: 'current_runs',
+      type: 'number',
+      hide: true,
+    },
+    {
+      field: 'current_pass_percentage',
+      headerName: 'Current pass percentage',
+      type: 'number',
+      flex: 0.75,
+      renderCell: (params) => (
+        <div className="percentage-cell">
+          {Number(params.value).toFixed(1).toLocaleString()}%<br />
+          <small>({params.row.current_runs} runs)</small>
+        </div>
+      ),
+    },
+    {
+      field: 'net_improvement',
+      headerName: 'Improvement',
+      type: 'number',
+      flex: 0.5,
+      renderCell: (params) => {
+        return <PassRateIcon tooltip={true} improvement={params.value} />
+      },
+    },
+    {
+      field: 'previous_pass_percentage',
+      headerName: 'Previous pass percentage',
+      flex: 0.75,
+      type: 'number',
+      renderCell: (params) => (
+        <div className="percentage-cell">
+          {Number(params.value).toFixed(1).toLocaleString()}%<br />
+          <small>({params.row.previous_runs} runs)</small>
+        </div>
+      ),
+    },
+  ]
 
-  const cards = []
-  const noData = [] // Put these at the end
-  variants.forEach((variant, index) => {
-    if (variant.current_runs === 0) {
-      noData.push(minicard(variant, index))
-    } else {
-      cards.push(minicard(variant, index))
-    }
-  })
+  const rows = variants.map((variant, index) => ({
+    id: index,
+    name: variant.name,
+    current_runs: variant.current_runs,
+    current_pass_percentage: variant.current_pass_percentage,
+    previous_runs: variant.previous_runs,
+    previous_pass_percentage: variant.previous_pass_percentage,
+    net_improvement: variant.net_improvement,
+  }))
 
   return (
-    <Fragment>
-      <Grid md={12} sm={12}>
-        <Typography variant="h5">
-          Variant status
-          <Tooltip title="Variant status shows the current and previous pass rates for each variant. A variant is jobs grouped by platform, SDN, architecture, etc.">
-            <Info />
-          </Tooltip>
-        </Typography>
-      </Grid>
-      <Accordion elevation={5} style={{ margin: 10, borderRadius: 5 }}>
-        <AccordionSummary
-          aria-controls="variant-content"
-          id="variant-header"
-          expandIcon={<ExpandMore />}
-        >
-          <Grid container spacing={3}>
-            {cards.slice(0, 6).map((card) => card)}
-          </Grid>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Grid container spacing={3} style={{ marginRight: 20 }}>
-            {cards.length > 6 ? cards.slice(6).map((card) => card) : ''}
-            {noData.map((card) => card)}
-          </Grid>
-        </AccordionDetails>
-      </Accordion>
-    </Fragment>
+    <Container>
+      <div style={{ height: '100%', width: '100%', marginTop: 25 }}>
+        <DataGrid
+          disableSelectionOnClick
+          className={gridClasses.root}
+          components={{ Toolbar: GridToolbar }}
+          checkboxSelection={false}
+          autoHeight={true}
+          initialState={{
+            filter: {
+              filterModel: {
+                items: [
+                  {
+                    columnField: 'current_runs',
+                    operatorValue: '>=',
+                    value: '7',
+                  },
+                ],
+              },
+            },
+            sorting: {
+              sortModel: [{ field: 'current_pass_percentage', sort: 'asc' }],
+            },
+          }}
+          rowHeight={70}
+          rows={rows}
+          columns={columns}
+          pageSize={7}
+          rowsPerPageOptions={[7, 15, 25]}
+          getRowClassName={(params) => {
+            let c =
+              'row-percent-' + Math.round(params.row.current_pass_percentage)
+            return classes[c]
+          }}
+        />
+      </div>
+    </Container>
   )
 }
 
+export default withStyles(generateClasses(JOB_THRESHOLDS))(VariantCards)
+
 VariantCards.propTypes = {
+  classes: PropTypes.object,
   release: PropTypes.string.isRequired,
 }
